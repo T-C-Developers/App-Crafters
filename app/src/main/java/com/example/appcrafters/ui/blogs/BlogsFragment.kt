1aa780446e51cb9c50f578930cd0a503926e079a
Thejan Weerasekara <92569392+ThejanB@users.noreply.github.com>
2025-03-28 14:43:19 +0530

    navbar structure and initial page fragments implemented

---
+++ app/src/main/java/com/example/appcrafters/ui/blogs/BlogsFragment.kt
@@ -0,0 +1,42 @@
+package com.example.appcrafters.ui.blogs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.appcrafters.databinding.FragmentBlogsBinding
+
+class BlogsFragment : Fragment() {
+
+    private var _binding: FragmentBlogsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val slideshowViewModel =
+            ViewModelProvider(this)[BlogsViewModel::class.java]
+
+        _binding = FragmentBlogsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textSlideshow
+        slideshowViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/example/appcrafters/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/appcrafters/ui/settings/SettingsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.appcrafters.ui.slideshow
+package com.example.appcrafters.ui.groups
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class SettingsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is settings Fragment"
     }
     val text: LiveData<String> = _text
 }
